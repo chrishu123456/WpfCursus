b6946574e6bdcd4dcde304cea36789a2b906f907
Chris Huys <chrishuys@live.com>
2017-01-30 09:38:30 +0100

    ButtonGebruikWindow

--- WpfCursus/ButtonGebruikWindow.xaml.cs
+++ WpfCursus/ButtonGebruikWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace WpfCursus
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ButtonGebruikWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ButtonGebruikWindow : Window
     {
-        public MainWindow()
+        public ButtonGebruikWindow()
         {
             InitializeComponent();
         }
